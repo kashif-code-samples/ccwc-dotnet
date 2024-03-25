ef020e64db2b6a2227f7f69fc1c7e14671db0d1b
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-03-25 14:58:36 +0000

    Count bytes and lines

--- wc-cs/Program.cs
+++ wc-cs/Program.cs
@@ -7,24 +7,61 @@ class Program
         var countBytesOption = new Option<bool>(
             name: "--bytes",
             description: "print the byte counts",
-            getDefaultValue: () => true);
+            getDefaultValue: () => false);
         countBytesOption.AddAlias("-c");
 
+        var countLinesOption = new Option<bool>(
+            name: "--lines",
+            description: "print the newline counts",
+            getDefaultValue: () => false);
+        countLinesOption.AddAlias("-l");
+        
         var fileArgument = new Argument<string>();
 
         var rootCommand = new RootCommand
         {
             countBytesOption,
+            countLinesOption,
             fileArgument
         };
 
         rootCommand.SetHandler(
-            (byteCountOption, fileArgumentValue) =>
+            (countBytes, countLines, fileArgumentValue) =>
             {
-                Console.WriteLine($"{byteCountOption} {fileArgumentValue}");
+                if (countBytes)
+                {
+                    var byteCount = GetByteCount(fileArgumentValue);
+                    Console.WriteLine($"{byteCount} {fileArgumentValue}");
+                }
+
+                if (countLines)
+                {
+                    var lineCount = GetLineCount(fileArgumentValue);
+                    Console.WriteLine($"{lineCount} {fileArgumentValue}");
+                }
             },
-            countBytesOption, fileArgument);
+            countBytesOption, countLinesOption, fileArgument);
 
         await rootCommand.InvokeAsync(args);
     }
+
+    static long GetByteCount(string filename)
+    {
+        var fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+        return fs.Length;
+    }
+
+    static long GetLineCount(string filename)
+    {
+        var reader = new StreamReader(new FileStream(filename, FileMode.Open, FileAccess.Read));
+        
+        var lineCount = 0L;
+        while (reader.ReadLine() != null)
+        {
+            lineCount++;
+        }
+        return lineCount;
+    }
 }
+
+record Counts(long ByteCount);
