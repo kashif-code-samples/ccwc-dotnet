be2e4878b2c1801acb8b570dfbbf1f48b312b7f7
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-03-25 00:29:32 +0000

    Setup project with System.CommandLine for parsing command line options

---
+++ wc-cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.CommandLine;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var countBytesOption = new Option<bool>(
+            name: "--bytes",
+            description: "print the byte counts",
+            getDefaultValue: () => true);
+        countBytesOption.AddAlias("-c");
+
+        var fileArgument = new Argument<string>();
+
+        var rootCommand = new RootCommand
+        {
+            countBytesOption,
+            fileArgument
+        };
+
+        rootCommand.SetHandler(
+            (byteCountOption, fileArgumentValue) =>
+            {
+                Console.WriteLine($"{byteCountOption} {fileArgumentValue}");
+            },
+            countBytesOption, fileArgument);
+
+        await rootCommand.InvokeAsync(args);
+    }
+}
